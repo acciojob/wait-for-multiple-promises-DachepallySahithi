4699f54218ca620bb235df40b82136b66477fd2c
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-31 18:07:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-//your JS code here. If required.
-function getRandomTIme() {
-	return(Math.random() * 2 + 1).toFixed(3);
-}
-const promises = [
-	new Promise(resolve => setTimeout(() => resolve({ name: "Promise 1", time: getRandomTIme() }), getRandomTIme() * 1000)),
-	new Promise(resolve => setTimeout(() => resolve({ name: "Promise 2", time: getRandomTime() }), getRandomTime() * 1000)),
-    new Promise(resolve => setTimeout(() => resolve({ name: "Promise 3", time: getRandomTime() }), getRandomTime() * 1000))
-];
-
-const startTime = performance.now();
+function getRandomTime() {
+        return (Math.random() * 2 + 1).toFixed(3);
+      }
+      
+      function createPromise(name) {
+        const time = getRandomTime();
+        return new Promise(resolve => setTimeout(() => resolve({ name, time }), time * 1000));
+      }
+      
+      const promises = [
+        createPromise("Promise 1"),
+        createPromise("Promise 2"),
+        createPromise("Promise 3")
+      ];
+      
+      const startTime = performance.now();
       
       Promise.all(promises).then(results => {
         const endTime = performance.now();
@@ -26,4 +31,6 @@ const startTime = performance.now();
         const totalRow = document.createElement("tr");
         totalRow.innerHTML = `<td>Total</td><td>${totalTime}</td>`;
         tableBody.appendChild(totalRow);
+      }).catch(error => {
+        console.error("Error in promises: ", error);
       });
